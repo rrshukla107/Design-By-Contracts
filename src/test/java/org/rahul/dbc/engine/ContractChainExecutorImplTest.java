5929986c914edb77181c3b3be8b8403f0dda96c0
Rahul Shukla <rrshukla107@gmail.com>
2020-10-24 17:27:10 -0700

    Completed the first draft of the Contract Engine
    - Adding more test cases

--- src/test/java/org/rahul/dbc/engine/ContractChainExecutorImplTest.java
+++ src/test/java/org/rahul/dbc/engine/ContractChainExecutorImplTest.java
@@ -18,7 +18,7 @@ public class ContractChainExecutorImplTest {
     private FlatContract<Object> contract1 = obj -> {
         try {
             System.out.println("Executing contract 1");
-            TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -57,6 +57,11 @@ public class ContractChainExecutorImplTest {
 
     private FlatContract<Object> contractWithRuntimeError = obj -> {
 
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         throw new RuntimeException("Sample Exception Message");
     };
 
@@ -110,6 +115,82 @@ public class ContractChainExecutorImplTest {
         Assert.assertEquals(chainResult.getUnderlyingException().get().getMessage(), "Sample Exception Message");
 
     }
+
+
+    @Test
+    public void runMultipleSuccessfulChainsParallel() {
+
+
+        List<SingleArgContractWrapper<Object>> chain = List.of(this.getWrapper("contract1", contract1, new Object())
+                , this.getWrapper("contract2", contract2, new Object())
+                , this.getWrapper("contract3", contract3, new Object()));
+
+
+        //FORK
+        CompletableFuture<ChainResult>[] promises = new CompletableFuture[]{
+                this.chainExecutor.executeChain(chain),
+                this.chainExecutor.executeChain(chain),
+                this.chainExecutor.executeChain(chain)};
+
+        try {
+            // JOIN
+            CompletableFuture.allOf(promises).get();
+
+            Assert.assertTrue(promises[0].get().getResult());
+            Assert.assertTrue(promises[1].get().getResult());
+            Assert.assertTrue(promises[2].get().getResult());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void runMultipleChainsInParallelWithFailure() {
+
+        List<SingleArgContractWrapper<Object>> successfulChain = List.of(this.getWrapper("contract1", contract1, new Object())
+                , this.getWrapper("contract2", contract2, new Object())
+                , this.getWrapper("contract3", contract3, new Object()));
+
+        List<SingleArgContractWrapper<Object>> chainWithFailure = List.of(this.getWrapper("contract1", contract1, new Object())
+                , this.getWrapper("contractFailure", contractFailure1, new Object())
+                , this.getWrapper("contract3", contract3, new Object()));
+
+        List<SingleArgContractWrapper<Object>> chainWithExceptions = List.of(this.getWrapper("contract1", contract1, new Object()),
+                this.getWrapper("contractException", this.contractWithRuntimeError, new Object()),
+                this.getWrapper("contract3", contract3, new Object()));
+
+
+        //FORK
+        CompletableFuture<ChainResult>[] promises = new CompletableFuture[]{
+                this.chainExecutor.executeChain(successfulChain),
+                this.chainExecutor.executeChain(chainWithFailure),
+                this.chainExecutor.executeChain(chainWithExceptions)};
+
+        try {
+            // JOIN
+            CompletableFuture.allOf(promises).get();
+
+            //All contracts successful in this chain
+            Assert.assertTrue(promises[0].get().getResult());
+
+            //Contract Failure in this Chain
+            Assert.assertFalse(promises[1].get().getResult());
+            Assert.assertEquals(promises[1].get().getFailedContractName().get(), "contractFailure");
+
+            //Failure due to Exception in this chain
+            Assert.assertFalse(promises[2].get().getResult());
+            Assert.assertEquals(promises[2].get().getFailedContractName().get(), "contractException");
+            Assert.assertEquals(promises[2].get().getUnderlyingException().get().getMessage(), "Sample Exception Message");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     private <ARG1> SingleArgContractWrapper<ARG1> getWrapper(final String name, final FlatContract<ARG1> contract, ARG1 object) {
         return new SingleArgContractWrapper<ARG1>(name, contract, object);
